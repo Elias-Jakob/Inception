4813bb4456e4567af8c68587db508bddda78c602
Elias Jakob <elias5.jakob@gmail.com>
2026-09-10 18:05:48 +0200

    feat: mariadb runs

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,1 +1,11 @@
+if [ ! -f /var/lib/mysql/data ]; then
+    mkdir -p /var/lib/mysql/data
+    chown -R mysql:mysql /var/lib/mysql
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld/
+    touch /run/mysqld/mysqld.sock
+    chown -R mysql:mysql /run/mysqld/mysqld.sock
+    mariadb-install-db --user=mysql \
+        --datadir=/var/lib/mysql
+fi
 exec mariadbd --user=mysql
