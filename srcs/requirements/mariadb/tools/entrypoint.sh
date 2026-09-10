1aeccefe14c8d88de5d3eb0486aee7861c4e98fd
Elias Jakob <elias5.jakob@gmail.com>
2026-09-10 11:24:44 +0200

    feat: start mariadb

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,1 @@
+exec mariadbd --user=mysql
--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -1,7 +1,8 @@
-#cd /var/www/html
-#cp wp-config-sample.php wp-config.php
-#sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '$MYSQL_DB_NAME' );/" wp-config.php
-#sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/" wp-config.php
-#sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$(cat /run/secrets/db_password)' );/" wp-config.php
+cd /var/www/html
+cp wp-config-sample.php wp-config.php
+sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '$MYSQL_DB_NAME' );/" wp-config.php
+sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/" wp-config.php
+sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$(cat /run/secrets/db_password)' );/" wp-config.php
+sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb' );/" wp-config.php
 
 exec /usr/sbin/php-fpm84 -F
